a0b3318381124abe5e1398a19322453419a0ba1c
mrMDA28 <45512200+mrmda28@users.noreply.github.com>
2020-06-23 03:49:50 +0300

    Update calendar and ideas

--- Calendar/js/script.js
+++ Calendar/js/script.js
@@ -1,1 +1,56 @@
-alert("dfsfdsfdsf")
+let dt = new Date();
+
+function renderDate() {
+    dt.setUTCDate(1);
+    let day = dt.getUTCDay();
+    let today = new Date();
+    let endDate = new Date(
+        dt.getUTCFullYear(),
+        dt.getUTCMonth() + 1,
+        0
+    ).getUTCDate() + 1;
+
+let prevDate = new Date(
+    dt.getUTCFullYear(),
+    dt.getUTCMonth(),
+    0
+).getUTCDate();
+let months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь"
+]
+document.getElementById("month").innerHTML = months[dt.getUTCMonth()];
+document.getElementById("year").innerHTML = dt.getUTCFullYear();
+
+let cells = "";
+for (x = day-2; x >= 0; x--) {
+    cells += "<div class='prev_date'>" + (prevDate - x + 1) + "</div>";
+}
+
+for (i = 1; i <= endDate; i++) {
+    if (i == today.getUTCDate() && dt.getUTCMonth() == today.getUTCMonth())
+        cells += "<div class='today'>" + i + "</div>";
+    else
+        cells += "<div>" + i + "</div>";
+}
+document.getElementsByClassName("days")[0].innerHTML = cells;
+}
+
+function moveDate(button) {
+    if (button == "prev") {
+        dt.setMonth(dt.getMonth() - 1);
+    } else if (button == 'next') {
+        dt.setMonth(dt.getMonth() + 1);
+    }
+    renderDate();
+}
