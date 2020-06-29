cb7e3b8376f9eb86c33e9276a57c8081e3762554
mrMDA28 <45512200+mrmda28@users.noreply.github.com>
2020-06-29 04:18:32 +0300

    Add new design

--- Calendar/js/script.js
+++ Calendar/js/script.js
@@ -29,21 +29,37 @@ let months = [
     "Ноябрь",
     "Декабрь"
 ]
+let prevmn = dt.getUTCMonth()-1,
+    nextmn = dt.getUTCMonth()+1;
 document.getElementById("month").innerHTML = months[dt.getUTCMonth()];
 document.getElementById("year").innerHTML = dt.getUTCFullYear();
+if (prevmn == -1) {
+    prevmn = 11;
+}
+document.getElementById("prevm").innerHTML = months[prevmn];
+if (nextmn == 12) {
+    nextmn = 0;
+}
+document.getElementById("nextm").innerHTML = months[nextmn];
 
 let cells = "";
 for (x = day-2; x >= 0; x--) {
-    cells += "<div class='prev_date'>" + (prevDate - x + 1) + "</div>";
+    cells += "<div class='prev_date' class='day' id='day'>" + (prevDate - x + 1) + "</div>";
 }
 
 for (i = 1; i <= endDate; i++) {
     if (i == today.getUTCDate() && dt.getUTCMonth() == today.getUTCMonth())
-        cells += "<div class='today'>" + i + "</div>";
+        cells += "<div class='today' id='day" + i + "' onclick='getDateEvents(id)'>" + i + "</div>";
     else
-        cells += "<div>" + i + "</div>";
+        cells += "<div id='day" + i + "' onclick='getDateEvents(id)'>" + i + "</div>";
 }
 document.getElementsByClassName("days")[0].innerHTML = cells;
+let merDate = document.getElementsByClassName("today")[0].innerHTML + "." + (dt.getUTCMonth()+1 + "." + dt.getUTCFullYear());
+document.getElementById("date-events").innerHTML = merDate; //Временно
+}
+
+function getDateEvents(id) {
+    document.getElementById("date-events").innerHTML = document.getElementById(id).innerHTML + "." + (dt.getUTCMonth()+1 + "." + dt.getUTCFullYear());
 }
 
 function moveDate(button) {
@@ -54,3 +70,4 @@ function moveDate(button) {
     }
     renderDate();
 }
+
